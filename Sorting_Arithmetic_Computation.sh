d886cc93de4b902f3b963f292ad6d89221875f53
IamSanketMore <sanketmore.999.m@gmail.com>
2020-12-14 20:36:39 +0530

    Add Sorting Arithmetic Computation UC5 Problem to UC5 Branch

--- Sorting_Arithmetic_Computation.sh
+++ Sorting_Arithmetic_Computation.sh
@@ -7,6 +7,7 @@ read -p "Enter Third Input :- " c
 sum1=0
 sum2=0
 sum3=0
+sum4=0
 echo "---------------- INPUTS ----------------------"
 echo "First Input Number is :- $a"
 echo "Second Input Number is :- $b"
@@ -24,3 +25,8 @@ echo "Sum is:- $sum2"
 echo "*********** Compute c + a / b ****************"
 sum3=$(($c+$a/$b))
 echo "Sum is:- $sum3"
+
+echo "*********** Compute a % b + c ****************"
+sum4=$(($a%$b+$c))
+echo "Sum is:- $sum4"
+
