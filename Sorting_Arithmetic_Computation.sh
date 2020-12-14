d66608514e4fff5eb2a069c05e09afd0ef7f7efe
IamSanketMore <sanketmore.999.m@gmail.com>
2020-12-14 19:37:12 +0530

    Add Sorting Arithmetic Computation UC1 Problem to UC1 Branch

--- Sorting_Arithmetic_Computation.sh
+++ Sorting_Arithmetic_Computation.sh
@@ -1,1 +1,10 @@
-echo "Welcome "
+#!/bin/bash
+
+read -p "Enter First Input :- " a
+read -p "Enter Second Input :- " b
+read -p "Enter Third Input :- " c
+
+echo "---------------- INPUTS ----------------------"
+echo "First Input Number is :- $a"
+echo "Second Input Number is :- $b"
+echo "Third Input Number is :- $c"
