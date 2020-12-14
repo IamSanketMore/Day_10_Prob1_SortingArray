b6ffb943e765dce9a1913096af545eaeecac00bb
IamSanketMore <sanketmore.999.m@gmail.com>
2020-12-14 20:18:04 +0530

    Add Sorting Arithmetic Computation UC2 Problem to UC2 Branch

--- Sorting_Arithmetic_Computation.sh
+++ Sorting_Arithmetic_Computation.sh
@@ -4,7 +4,13 @@ read -p "Enter First Input :- " a
 read -p "Enter Second Input :- " b
 read -p "Enter Third Input :- " c
 
+sum1=0
+
 echo "---------------- INPUTS ----------------------"
 echo "First Input Number is :- $a"
 echo "Second Input Number is :- $b"
 echo "Third Input Number is :- $c"
+
+echo "*********** Compute a + b * c ****************"
+sum1=$(( $a+$b*$c ))
+echo "Sum is:- $sum1"
