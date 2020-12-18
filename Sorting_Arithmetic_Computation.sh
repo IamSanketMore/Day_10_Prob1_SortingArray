298a72559417fbf4789ff93b891a8b7b404b5fd2
IamSanketMore <sanketmore.999.m@gmail.com>
2020-12-18 14:24:24 +0530

    Add Sorting Arithmetic Computation UC6 Problem to UC6 Branch

--- Sorting_Arithmetic_Computation.sh
+++ Sorting_Arithmetic_Computation.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
 
+declare -A CompResult
 read -p "Enter First Input :- " a
 read -p "Enter Second Input :- " b
 read -p "Enter Third Input :- " c
 
-sum1=0
-sum2=0
-sum3=0
-sum4=0
 echo "---------------- INPUTS ----------------------"
 echo "First Input Number is :- $a"
 echo "Second Input Number is :- $b"
 echo "Third Input Number is :- $c"
 
-echo "*********** Compute a + b * c ****************"
+echo "*********** 1] Compute a + b * c ****************"
 sum1=$(( $a+$b*$c ))
 echo "Sum is:- $sum1"
 
 
-echo "*********** Compute a * b + c ****************"
+echo "*********** 2] Compute a * b + c ****************"
 sum2=$(($a*$b+$c))
 echo "Sum is:- $sum2"
 
-echo "*********** Compute c + a / b ****************"
+echo "*********** 3] Compute c + a / b ****************"
 sum3=$(($c+$a/$b))
 echo "Sum is:- $sum3"
 
-echo "*********** Compute a % b + c ****************"
+echo "*********** 4] Compute a % b + c ****************"
 sum4=$(($a%$b+$c))
 echo "Sum is:- $sum4"
+
+#---------Store the computation in a Dictionary------------------------------------
+CompResult[1]="$sum1"
+CompResult[2]="$sum2"
+CompResult[3]="$sum3"
+CompResult[4]="$sum4"
 
